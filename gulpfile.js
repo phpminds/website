03bac1932f8374bd6a1b4239864bb02c196808bc
Shaun Hare <shaun.hare@nottingham.ac.uk>
2015-11-03 07:29:24 +0000

    added sourcemaps and thrown in the crappy mvp styles from before to check

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('sass', function () {
   gulp.src('./build/sass/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./public/css'));
 });
  

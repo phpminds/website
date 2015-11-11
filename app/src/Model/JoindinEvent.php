7542032d07dc860f925ce5fdf2bc767ac5f791c4
Antonis Pavlakis <antonis@pavlakis.info>
2015-11-11 09:14:11 +0000

    [Joindin] Pass the event ID through the location provided in the headers.

--- app/src/Model/JoindinEvent.php
+++ app/src/Model/JoindinEvent.php
@@ -8,16 +8,20 @@ class JoindinEvent
 {
     private $apiKey;
     private $baseUrl;
+    private $callbackUrl;
     private $token;
+
+    private $eventLocation;
 
     private $talkID;
     private $talkURL;
 
-    public function __construct($apiKey, $baseUrl, $token)
+    public function __construct($apiKey, $baseUrl, $callback, $token)
     {
-        $this->apiKey = $apiKey;
-        $this->baseUrl = $baseUrl;
-        $this->token = $token;
+        $this->apiKey       = $apiKey;
+        $this->baseUrl      = $baseUrl;
+        $this->callbackUrl  = $callback;
+        $this->token        = $token;
     }
 
     public function setTalkID($talkID)
@@ -29,7 +33,6 @@ class JoindinEvent
     {
         $this->talkURL = $url;
     }
-
 
     public function getHeaders()
     {
@@ -67,6 +70,36 @@ class JoindinEvent
         ];
     }
 
+    public function getCreateEventTitlePayload(Event $event, $language = 'English - UK')
+    {
+//        echo $event->getTalk()->getSpeaker();
+//        echo PHP_EOL;
+//        exit;
+        // create talk
+        // {"talk_title":"The first ever talk which works.","talk_description":"the first description","language":"English - UK","talk_type":"Talk","start_date":"2015-12-17T12:19:00+00:00","speakers":["Antonios Pavlakis"]}
+
+
+        $speakers = [$event->getTalk()->getSpeaker()->getFirstName() . ' ' . $event->getTalk()->getSpeaker()->getLastName()];
+
+        $payload = [
+            'talk_title' => $event->getTalk()->getTitle(),
+            'talk_description' => $event->getTalk()->getDescription(),
+            'language' => $language,
+            'talk_type' => 'Talk',
+            'start_date' => $event->getDate()->setTimezone(new \DateTimeZone( 'UTC' ))->format('Y-m-d H:i:s'),
+            'speakers' => $speakers
+
+        ];
+
+
+////
+//        print_r(json_encode($payload));
+//        echo PHP_EOL;
+//        exit;
+
+        return $payload;
+    }
+
     public function getBaseUrl()
     {
         return $this->baseUrl;
@@ -80,5 +113,32 @@ class JoindinEvent
     public function getTalkUrl()
     {
         return $this->talkURL;
+    }
+
+    /**
+     * @param $eventLocation
+     */
+    public function setEventLocation($eventLocation)
+    {
+        echo $eventLocation . ' <<<' . PHP_EOL;
+        $this->eventLocation = $eventLocation;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventLocation()
+    {
+        return $this->eventLocation;
+    }
+
+    public function getJoindinEventID()
+    {
+        $id = substr($this->getEventLocation(), strlen($this->baseUrl . '/events/'));
+        if (substr($id, -1) == '/') {
+            return substr($id, 0, strlen($id) - 1);
+        }
+
+        return $id;
     }
 }

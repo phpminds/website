bff4a7d4a4831add45efaf2e2e58c7c584bf9d63
Antonis Pavlakis <antonis@pavlakis.info>
2015-11-12 22:17:11 +0000

    Throw custom exception.

--- app/src/Model/Email.php
+++ app/src/Model/Email.php
@@ -2,19 +2,27 @@
 
 namespace App\Model;
 
+use App\Exception\Model\InvalidEmailException;
+
 class Email
 {
+    /**
+     * @var String
+     */
     private $email;
 
     public function __construct($email)
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new \Exception('Email ' . $email . ' not valid.');
+            throw new InvalidEmailException('Email ' . $email . ' not valid.');
         }
 
         $this->email = $email;
     }
 
+    /**
+     * @return String
+     */
     public function __toString()
     {
         return $this->email;

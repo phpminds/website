066cce0d00815503a49f051ce0595db46e7b1ab1
Antonis Pavlakis <antonis@pavlakis.info>
2015-11-15 22:34:34 +0000

    [Docs] Updated block comments.

--- app/src/Action/HomeAction.php
+++ app/src/Action/HomeAction.php
@@ -1,16 +1,28 @@
 <?php
 namespace App\Action;
 
+use App\Service\EventsService;
 use Slim\Views\Twig;
 use Psr\Log\LoggerInterface;
 
 final class HomeAction
 {
+    /**
+     * @var Twig
+     */
     private $view;
+
+    /**
+     * @var LoggerInterface
+     */
     private $logger;
+
+    /**
+     * @var EventsService
+     */
     private $eventService;
 
-    public function __construct(Twig $view, LoggerInterface $logger, $eventService)
+    public function __construct(Twig $view, LoggerInterface $logger, EventsService $eventService)
     {
         $this->view = $view;
         $this->logger = $logger;

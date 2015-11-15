779d223d504fd650843d63e419f75239c89426ca
Antonis Pavlakis <antonis@pavlakis.info>
2015-11-15 17:05:56 +0000

    Added custom exceptions.

---
+++ app/src/Exception/Model/InvalidPersonFirstName.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace App\Exception\Model;
+
+
+class InvalidPersonFirstName extends \Exception {}
---
+++ app/src/Exception/Model/InvalidPersonLastName.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace App\Exception\Model;
+
+
+class InvalidPersonLastName extends \Exception {}

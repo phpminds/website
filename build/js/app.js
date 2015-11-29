2660e7f274e1ffd26f0bc50d565f90e569bcd1da
Shaun Hare <shaun.hare@nottingham.ac.uk>
2015-11-29 10:14:56 +0000

    Added login form styling

--- build/js/app.js
+++ build/js/app.js
@@ -128,3 +128,15 @@
 })(window.phpminds = window.phpminds || {}, jQuery);
 
 
+$(window).bind("load", function () {
+    var footer = $("footer");
+    var pos = footer.position();
+    var height = $(window).height();
+    height = height - pos.top;
+    height = height - footer.height();
+    if (height > 0) {
+        footer.css({
+            'margin-top': height + 'px'
+        });
+    }
+});

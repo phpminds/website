616449cba56f12fe24e9521a146597ed069f0d39
Antonis Pavlakis <antonis@pavlakis.info>
2015-12-12 20:53:49 +0000

    [DB] Wrapped db calls through the manager.

--- app/src/Model/Event/EventManager.php
+++ app/src/Model/Event/EventManager.php
@@ -3,6 +3,7 @@
 namespace App\Model\Event;
 
 use App\Model\Event\Entity\Speaker;
+use App\Model\Event\Entity\Event AS EventEntity;
 use App\Repository\EventsRepository;
 use App\Repository\SpeakersRepository;
 use App\Repository\SupportersRepository;
@@ -49,6 +50,9 @@ class EventManager
         return $this->speakersRepo->getBySpeakerID($speakerID);
     }
 
+    /**
+     * @return array
+     */
     public function getSupporters()
     {
         return $this->supportersRepo->getAllSupporters();
@@ -63,9 +67,49 @@ class EventManager
         return $this->supportersRepo->getSupporterByID($supporterID);
     }
 
+    /**
+     * @param $meetupID
+     * @return array
+     */
     public function getDetailsByMeetupID($meetupID)
     {
         return $this->eventsRepo->getByMeetupID($meetupID);
+    }
+
+    /**
+     * @param EventEntity $event
+     */
+    public function saveEvent(EventEntity $event)
+    {
+        $this->eventsRepo->save($event);
+    }
+
+    /**
+     * @param $eventName
+     * @return bool
+     */
+    public function eventExists($eventName)
+    {
+        return $this->eventsRepo->eventExists($eventName);
+    }
+
+    /**
+     * @param $meetupID
+     * @return bool
+     */
+    public function getByMeetupID($meetupID)
+    {
+        return $this->eventsRepo->$this->getByMeetupID($meetupID);
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllPendingEvents()
+    {
+        $pendingEvents = $this->eventsRepo->getAllPending();
+
+        return $pendingEvents;
     }
 
 }

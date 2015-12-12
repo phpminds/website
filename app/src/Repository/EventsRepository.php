864361f69784d31b16432c805ce6a06df4051192
Antonis Pavlakis <antonis@pavlakis.info>
2015-12-12 20:55:19 +0000

    [DB] Save now checks if it should insert or update.

--- app/src/Repository/EventsRepository.php
+++ app/src/Repository/EventsRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Model\Event\Entity\Venue;
 use App\Repository\RepositoryAbstract;
 
 use App\Model\Event\Entity\Event;
@@ -23,9 +24,14 @@ class EventsRepository extends RepositoryAbstract
 
     /**
      * @param Event $event
+     * @return bool
      */
     public function save(Event $event)
     {
+        if ($this->eventExists($event->getJoindinEventName())) {
+            return $this->update($event);
+        }
+
         $sql = "INSERT INTO {$this->table} (meetup_id, meetup_venue_id, joindin_event_name, joindin_talk_id, joindin_url, speaker_id, supporter_id) VALUES ("
             . ":meetup_id, :meetup_venue_id, :joindin_event_name, :joindin_talk_id, :joindin_url, :speaker_id, :supporter_id"
             . ")";
@@ -43,6 +49,27 @@ class EventsRepository extends RepositoryAbstract
         $stmt->execute();
 
         $event->id = $this->db->lastInsertId();
+    }
+
+    public function update(Event $event)
+    {
+        $sql = 'UPDATE ' . $this->table
+            . ' SET meetup_venue_id = :meetup_venue_id, joindin_event_name = :joindin_event_name,'
+            . ' joindin_talk_id = :joindin_talk_id, joindin_url = :joindin_url, speaker_id = :speaker_id,'
+            . ' supporter_id = :supporter_id'
+            . ' WHERE meetup_id = :meetup_id';
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(":meetup_id", $event->getMeetupID(), \PDO::PARAM_INT);
+        $stmt->bindParam(":meetup_venue_id", $event->getMeetupVenueID(), \PDO::PARAM_INT);
+        $stmt->bindParam(":joindin_event_name", $event->getJoindinEventName(), \PDO::PARAM_STR);
+        $stmt->bindParam(":joindin_talk_id", $event->getJoindinTalkID(), \PDO::PARAM_INT);
+        $stmt->bindParam(":joindin_url", $event->getJoindinURL(), \PDO::PARAM_STR);
+        $stmt->bindParam(":speaker_id", $event->getSpeakerID(), \PDO::PARAM_INT);
+        $stmt->bindParam(":supporter_id", $event->getSupporterID(), \PDO::PARAM_INT);
+
+
+        return $stmt->execute();
     }
 
     public function getByMeetupID($meetupID)
@@ -97,7 +124,7 @@ class EventsRepository extends RepositoryAbstract
     public function getAllPending()
     {
         // a pending event has a joindin_talk_id of ZERO
-        $sql = 'SELECT meetup_id, joindin_event_name'
+        $sql = 'SELECT meetup_id, joindin_event_name, speaker_id, supporter_id'
             . ' FROM '. $this->table
             . ' WHERE joindin_talk_id = 0 ';
 

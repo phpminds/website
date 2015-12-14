a58d665e5ca20335d7c828aadbe1025ae7d18a55
Shaun Hare <shaun.hare@nottingham.ac.uk>
2015-12-14 23:58:28 +0000

    [travis] - fire

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git remote add production ssh://travis@phpminds.org/home/travis/phpminds.git
-git push production develop 
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push production develop
 
 return 0

182236a951769b5c715e39fdfe28be9dcd90835c
Shaun Hare <shaun.hare@nottingham.ac.uk>
2015-12-15 00:05:53 +0000

    [travis] oops still on develop

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git remote add production ssh://travis@phpminds.org/home/travis/phpminds.git
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push production develop
+git push production develop
 
 return 0

c5f03b51b8a442b7f1e5e13b77a9244abb794789
Antonis Pavlakis <antonis@pavlakis.info>
2015-12-21 22:53:44 +0000

    [Test] Initial tests for meetup event.

---
+++ tests/phpunit/Model/MeetupEventTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\Model;
+
+
+use App\Model\Email;
+use App\Model\Event\Entity\Speaker;
+use App\Model\Event\Entity\Supporter;
+use App\Model\Event\Entity\Talk;
+use App\Model\Event\Entity\Venue;
+use App\Model\Event\Event;
+use App\Model\Twitter;
+
+class MeetupEventTest extends \App\Tests\Helper
+{
+
+    /**
+     * @var \App\Model\MeetupEvent
+     */
+    protected $meetupEvent;
+
+    /**
+     * @var Event
+     */
+    protected $event;
+
+    public function setUp()
+    {
+        $settings = $this->getSettings();
+
+        $meetup = $settings['settings']['meetups'];
+
+        $meetupConfig = new \App\Config\MeetupConfig(
+            [
+                'apiKey'        => $meetup['apiKey'],
+                'baseUrl'       => $meetup['baseUrl'],
+                'groupUrlName'  => $meetup['PHPMinds']['group_urlname'],
+                'publishStatus' => $meetup['publish_status']
+            ]
+        );
+
+        $this->meetupEvent = new \App\Model\MeetupEvent($meetupConfig);
+
+        $this->event = $this->getEvent();
+    }
+
+    public function testCanCreateAnEventPayload()
+    {
+        $eventPayload = $this->meetupEvent->getCreateEventPayload($this->event);
+    }
+}

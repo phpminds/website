d0f0c85ac182e7ae8f960bc2b9cb41c44266c179
Antonis Pavlakis <antonis@pavlakis.info>
2015-12-27 01:36:59 +0000

    Pass full namespace as container key.

--- app/middleware.php
+++ app/middleware.php
@@ -5,11 +5,10 @@
 $app->add(new \Slim\HttpCache\Cache('public', 86400));
 
 
-$app->add($container->get('auth.middleware'));
 
-$app->add($container->get('csrf'));
+$app->add($container->get('Slim\Csrf\Guard'));
 
-$app->add($container->get('auth.middleware'));
+$app->add($container->get('PHPMinds\Middleware\AuthCheck'));
 
 $app->add(new PHPMinds\Middleware\CliRequest());
 

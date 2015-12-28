4971a6cfda7a8617d6c139d357d1fa9581be3e0f
Antonis Pavlakis <antonis@pavlakis.info>
2015-12-28 17:41:34 +0000

    Replaced cli request middleware with external library.

--- app/middleware.php
+++ app/middleware.php
@@ -11,5 +11,4 @@ $app->add($container->get('csrf'));
 
 $app->add($container->get('auth.middleware'));
 
-$app->add(new PHPMinds\Middleware\CliRequest());
-
+$app->add(new \pavlakis\cli\CliRequest());

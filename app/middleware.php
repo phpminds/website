23133bdf94e0eed5ad2ecb184046f29ae9afcf08
Antonios Pavlakis <antonis@pavlakis.info>
2017-04-17 23:46:13 +0100

    Add CSP header through middleware

--- app/middleware.php
+++ app/middleware.php
@@ -2,6 +2,15 @@
 // Application middleware
 
 
+$app->add(function($request, $response, $next) use ($container) {
+
+    $cspResponse = $response->withAddedHeader('Content-Security-Policy', $container->get('content-security-policies'));
+
+    return $next($request, $cspResponse);
+});
+
+
+
 $app->add(new \Slim\HttpCache\Cache('public', 86400));
 
 $app->add($container->get('Slim\Csrf\Guard'));

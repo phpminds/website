b02bbe6aa2ec4c5fe154f9383fd91e9f3fef82b4
Adoni Pavlakis <pavlakis@users.noreply.github.com>
2017-04-22 19:12:37 +0100

    Feature/csp (#77)
    * Add csp builder
    
    * Configure initial CSP settings
    
    * Add nonce to inline scripts
    
    * Add CSP header through middleware
    
    * Allow style-src unsafe-inline to fix site
    
    * CSP - do not allow inline styles
    
    * Add consistency with container key naming

--- app/middleware.php
+++ app/middleware.php
@@ -2,6 +2,15 @@
 // Application middleware
 
 
+$app->add(function($request, $response, $next) use ($container) {
+
+    $cspResponse = $response->withAddedHeader('Content-Security-Policy', $container->get('csp.config'));
+
+    return $next($request, $cspResponse);
+});
+
+
+
 $app->add(new \Slim\HttpCache\Cache('public', 86400));
 
 $app->add($container->get('Slim\Csrf\Guard'));

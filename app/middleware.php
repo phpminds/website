14944ba69a602b4e961653f733cbe871a0f24d5c
Shaun Hare <shaun@shaunhare.co.uk>
2017-06-22 22:09:58 +0100

    Develop (#89)
    * Add CSP middleware (#81)
    
    * Hotfix/csp broken admin (#83)
    
    * Fix notice on array access
    
    * Enable CSP in report-only mode in dashboard
    
    * Add comment for CSP override in routing
    
    * Admin Fixes (#85)
    
    * Add CSP middleware (#81) (#82)
    
    * Add CSP middleware (#81) (#84)
    
    * Hotfix/csp broken admin (#86)
    
    * Fix notice on array access
    
    * Enable CSP in report-only mode in dashboard
    
    * Add comment for CSP override in routing
    
    * Hotfix/csp broken admin (#88)
    
    * Fix notice on array access
    
    * Enable CSP in report-only mode in dashboard
    
    * Add comment for CSP override in routing
    
    * Add CSP middleware specifically in grouped routes only
    
    * Remove CSP middleware from app middleware

--- app/middleware.php
+++ app/middleware.php
@@ -3,7 +3,6 @@
 
 $app->add(new Pavlakis\Middleware\Csp\CspMiddleware($container->get('csp.config'), false));
 
-
 $app->add(new \Slim\HttpCache\Cache('public', 86400));
 
 $app->add($container->get('Slim\Csrf\Guard'));
